ad305c5f03c7496a035753785fb0bfac2449984a
Anandu B Ajith <reach2anandu@gmail.com>
2020-11-05 13:47:17 +0530

    add dueFeature

--- src/due/due.module.ts
+++ src/due/due.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DueService } from './service/due.service';
 import { DueController } from './controller/due.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DueEntity } from './model/due.entity';
+import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([DueEntity]),
+    UserModule,
+    AuthModule,
+  ],
   providers: [DueService],
   controllers: [DueController],
 })
