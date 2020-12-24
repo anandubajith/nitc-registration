fc0a019580b79433fb6d3068b58df48d6a9779c3
Anandu B Ajith <reach2anandu@gmail.com>
2020-12-24 23:30:43 +0530

    uppercase username

--- migrate.sql
+++ migrate.sql
@@ -23,18 +23,37 @@ insert into user_entity("username","name", "email", "password", "role", "contact
 insert into payment_entity("transactionId", "amount", "paymentDate", "bank", "modeOfPayment", "updated_at")
                 values
                     ('TXN123', '64646', '24/12/2020', 'SBI', 'online', NOW()),
-                    ('TXN123', '64646', '24/12/2020', 'SBI', 'online', NOW()),
-                    ('TXN123', '64646', '24/12/2020', 'SBI', 'online', NOW()),
-                    ('TXN123', '64646', '24/12/2020', 'SBI', 'online', NOW()),
-                    ('TXN123', '64646', '24/12/2020', 'SBI', 'online', NOW());
+                    ('TXN423', '64646', '24/12/2020', 'SBI', 'online', NOW()),
+                    ('TXN623', '64646', '24/12/2020', 'SBI', 'online', NOW()),
+                    ('TXN923', '64646', '24/12/2020', 'SBI', 'online', NOW()),
+                    ('TXN023', '64646', '24/12/2020', 'SBI', 'online', NOW());
 
 insert into verification_entity(updated_at)
                 values 
                     (NOW()),(NOW()),(NOW()),(NOW()),(NOW());
 
-insert into application_entity(status, submission_date, ownerId, paymentId, verificationId) 
-                        values('pending', NOW(), 1,1,1,1);
+insert into application_entity(status, submission_date, "ownerId", "paymentId", "verifiedBy","verificationStatusId") 
+                        values('pending', NOW(), 1,1,1,1), ('pending', NOW(), 2,2,2,2), ('pending', NOW(),3,3,3,3), ('pending', NOW(),4 ,4,4,4);
 
-id | transactionId | amount |          paymentDate          | bank | modeOfPayment |         updated_at
 
-id | sacId | facultyId | academicId | remark  |         updated_at
+insert into user_entity("username","name", "email", "password", "role", "contactNumber","semester" , "department", "category" )
+                values
+                    ('fa_1@nitc.ac.in', 'John Doe', 'fa_1@nitc.ac.in', '$2b$12$1c.him1xTJ7kRdekdLpcG.3fTEgN.c8gGCpiQfYbevEo1icDnngve', 'fa', '9497389579', '', 'Computer Science', 'General'),
+                     ('fa_2@nitc.ac.in', 'Jane Doe', 'fa_2@nitc.ac.in', '$2b$12$1c.him1xTJ7kRdekdLpcG.3fTEgN.c8gGCpiQfYbevEo1icDnngve', 'fa', '9497389579', '', 'Computer Science', 'General'),
+                      ('fa_3@nitc.ac.in', 'Tony', 'fa_3@nitc.ac.in', '$2b$12$1c.him1xTJ7kRdekdLpcG.3fTEgN.c8gGCpiQfYbevEo1icDnngve', 'fa', '9497389579', '', 'Computer Science', 'General');
+
+insert into user_entity("username","name", "email", "password", "role", "contactNumber","semester" , "department", "category" )
+                values
+                    ('student_1@nitc.ac.in', 'Student1', 'student_1@nitc.ac.in', '$2b$12$1c.him1xTJ7kRdekdLpcG.3fTEgN.c8gGCpiQfYbevEo1icDnngve', 'sac_admin', '9497389579', '', 'Computer Science', 'General');
+
+insert into user_entity("username","name", "email", "password", "role", "contactNumber","semester" , "department", "category" )
+                values
+                    ('academic@nitc.ac.in', 'Student1', 'academic@nitc.ac.in', '$2b$12$1c.him1xTJ7kRdekdLpcG.3fTEgN.c8gGCpiQfYbevEo1icDnngve', 'academic_admin', '9497389579', '', '', '');
+
+insert into user_entity("username","name", "email", "password", "role", "contactNumber","semester" , "department", "category" )
+                values
+                    ('library@nitc.ac.in', 'LibraryAdmin', 'library@nitc.ac.in', '$2b$12$1c.him1xTJ7kRdekdLpcG.3fTEgN.c8gGCpiQfYbevEo1icDnngve', 'library_admin', '9497389579', '', '', '');
+
+insert into user_entity("username","name", "email", "password", "role", "contactNumber","semester" , "department", "category" )
+                values
+                    ('hostel@nitc.ac.in', 'HostelAdmin', 'hostel@nitc.ac.in', '$2b$12$1c.him1xTJ7kRdekdLpcG.3fTEgN.c8gGCpiQfYbevEo1icDnngve', 'hostel_admin', '9497389579', '', '', '');
